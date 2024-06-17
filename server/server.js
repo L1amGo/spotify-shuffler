4db408c0ff0367d56eb80fc68e31df7a7d7cd68f
L1amGo <lguinto@uwaterloo.ca>
2024-06-17 04:06:29 -0400

    added login auth, player and tracklist view

---
+++ server/server.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const spotifyWebApi = require('spotify-web-api-node');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/refresh', (req, res) => {
+    const refreshToken = req.body.refreshToken;
+    const spotifyApi = new spotifyWebApi({
+        redirectUri: 'http://localhost:3000',
+        clientId: '2136706f70154b709bd7e932406f1e74',
+        clientSecret: '5105a1ebc3b34631b5893dc81599a530',
+        refreshToken
+    })
+    spotifyApi.refreshAccessToken().then(
+        (data) => {
+          console.log(data.body)
+        }).catch(() => {
+            res.sendStatus(400)
+        })
+})
+
+app.post('/login', (req, res) => {
+    const code = req.body.code;
+    const spotifyApi = new spotifyWebApi({
+        redirectUri: 'http://localhost:3000',
+        clientId: '2136706f70154b709bd7e932406f1e74',
+        clientSecret: '5105a1ebc3b34631b5893dc81599a530'
+    })
+
+    spotifyApi.authorizationCodeGrant(code).then(data => {
+        const accessToken = data.body.access_token;
+        const refreshToken = data.body.refresh_token;
+        res.json({
+            accessToken,
+            refreshToken,
+            expiresIn: data.body.expires_in,
+        })
+    })
+    .catch((err) => {
+        console.log(err)
+        res.sendStatus(400)
+    })
+})
+
+app.listen(3001, () => {
+    console.log('Listening on port 3001')
+})
